1965a5f586b47ebd653e5512a14646f9d370e058
OmarMuhammedAli <omarmuhammed1998.om@gmail.com>
2023-05-13 18:44:04 +0200

    feat: create api server

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/OmarMuhammedAli/FinGo/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Create Server struct with a store and a router
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// Create a function to return a new Server instance
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// Declare routes here
+
+	server.router = router
+	return server
+}
+
+// Create a function to start a server
+
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
+// Create a generic error response function to serialize errors
+func errorResponse(e error) gin.H {
+	return gin.H{"error": e.Error()}
+}

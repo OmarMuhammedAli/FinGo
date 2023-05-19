94fba71ab055e8834cfa913d357bfb5cbfe83705
OmarMuhammedAli <omarmuhammed1998.om@gmail.com>
2023-05-19 16:45:07 +0200

    feat: get account and list accounts

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	db "github.com/OmarMuhammedAli/FinGo/db/sqlc"
@@ -8,7 +9,7 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding="required"`
+	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,oneof=USD EGP EUR"`
 }
 
@@ -32,4 +33,53 @@ func (s *Server) createAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (s *Server) getAccount(ctx *gin.Context) {
+	var r getAccountRequest
+	if err := ctx.ShouldBindUri(&r); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	account, err := s.store.GetAccount(ctx, r.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountsRequest struct {
+	PageNo   int32 `form:"page_no" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=50"`
+}
+
+func (s *Server) listAccounts(ctx *gin.Context) {
+	var r listAccountsRequest
+	if err := ctx.ShouldBindQuery(&r); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  r.PageSize,
+		Offset: (r.PageNo - 1) * r.PageSize,
+	}
+	accounts, err := s.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+
 }

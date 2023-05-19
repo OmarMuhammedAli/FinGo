25741e2ca160a96153dd2508abbbfa30a5e60e47
OmarMuhammedAli <omarmuhammed1998.om@gmail.com>
2023-05-19 22:22:26 +0200

    feat: update and delete account

--- api/account.go
+++ api/account.go
@@ -3,6 +3,7 @@ package api
 import (
 	"database/sql"
 	"net/http"
+	"strconv"
 
 	db "github.com/OmarMuhammedAli/FinGo/db/sqlc"
 	"github.com/gin-gonic/gin"
@@ -82,4 +83,55 @@ func (s *Server) listAccounts(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, accounts)
 
+}
+
+type updateAccountRequest struct {
+	Balance int64 `json:"balance" binding:"required"`
+}
+
+func (s *Server) updateAccount(ctx *gin.Context) {
+	var r updateAccountRequest
+	idStr := ctx.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&r); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateAccountParams{
+		ID:      id,
+		Balance: r.Balance,
+	}
+	account, err := s.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (s *Server) deleteAccount(ctx *gin.Context) {
+	var r deleteAccountRequest
+	if err := ctx.ShouldBindUri(&r); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := s.store.DeleteAccount(ctx, r.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Account deleted successfully"})
 }
